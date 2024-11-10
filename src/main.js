d20450509608a7e2feb0ea2a3afdf7a8599d50c1
ttieman <tiemantanner@gmail.com>
2024-11-10 01:19:39 -0700

    explore creating objects and scenes

--- src/main.js
+++ src/main.js
@@ -1,16 +1,50 @@
+// main.js
 import { SceneManager } from "./objects/SceneManager";
+import * as THREE from "three";
+import * as RAPIER from '@dimforge/rapier3d/rapier';
 
-function main() {
-    const manager = SceneManager.getInstance();
+async function main() {
+    // Get the scene manager instance
+    const sceneManager = SceneManager.getInstance();
+    
+    // Create initial scene
+    sceneManager.createScene('mainScene', { includeDice: true });
+    
 
-    if(!manager.checkWebGLSupport()) {
-        return;
+    await sceneManager.loadScene('mainScene');
+   
+    
+        const currentSceneData = sceneManager.scenes.get('mainScene');
+
+        if (currentSceneData  && currentSceneData.config.includeDice == true) {
+            console.log(currentSceneData, "this exists")
+            // Create a new sphere
+            const sphereGeometry = new THREE.SphereGeometry(0.5);
+            const sphereMaterial = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
+            const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+            
+           
+           const sphereColliderDesc = RAPIER.ColliderDesc.ball(1);
+           const sphereRigidBodyDesc = RAPIER.RigidBodyDesc.dynamic();
+           
+           const sphereBody = sceneManager.physicsWorld.createRigidBody(sphereRigidBodyDesc);
+           
+           const sphereCollider = sceneManager.physicsWorld.createCollider(sphereColliderDesc,sphereBody);
+           
+                // Add to scene tracking
+            currentSceneData.objects.set('sphere', sphere);
+            currentSceneData.physicsObjects.set('sphere', {
+                mesh: sphere,
+                body: sphereBody,
+                collider: sphereCollider
+            });
+
+    
+
+            currentSceneData.scene.add(sphere);
+            
+        }
+
     }
 
-    manager.createScene("mainScene", { includeDice: true});
-
-    manager.loadScene("mainScene");
-}
-
-main();
-
+main().catch(console.error);
